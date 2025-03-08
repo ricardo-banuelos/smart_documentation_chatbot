6f78c6a1414da15a30a67844f9ccc9e40d9d174d
ricardo-banuelos <ricardo.banuelos.se@gmail.com>
2025-03-08 12:12:10 -0800

    save files with unique name

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI, UploadFile, File
+
 import os
+import uuid
 
 app = FastAPI()
 
 UPLOAD_DIR = "uploads"
 os.makedirs(UPLOAD_DIR, exist_ok=True)
+
+def get_file_extension(filename: str):
+    parts = os.path.splitext(filename)
+    if len(parts) > 1:
+        return parts[1]
+    return ""
 
 @app.get("/")
 async def root():
@@ -14,12 +22,15 @@ async def root():
 
 @app.post("/upload")
 async def upload_file(file: UploadFile = File(...)):
-    file_path = os.path.join(UPLOAD_DIR, file.filename)
+    file_extension = get_file_extension(file.filename)
+    unique_file_name = f'{uuid.uuid4()}{file_extension}'
+
+    file_path = os.path.join(UPLOAD_DIR, unique_file_name)
     with open(file_path, 'wb') as buffer:
         buffer.write(await file.read())
 
     return {
-        "file_name": file.filename,
+        "file_name": unique_file_name,
         "message": "File uploaded successfully!"
     }
 
